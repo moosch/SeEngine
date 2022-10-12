0ea4a10eb64faf9ca8e796cd2ec5b24ed1b8fdbb
moosch <mail@moosch.io>
2022-10-12 19:34:37 +1030

    Setup VkInstance

--- main.c
+++ main.c
@@ -5,24 +5,35 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+
 const char* WIN_TITLE = "SeEngine";
-const uint32_t WIN_WIDTH = 800;
-const uint32_t WIN_HEIGHT = 600;
+const u32 WIN_WIDTH = 800;
+const u32 WIN_HEIGHT = 600;
 
 typedef struct App {
   GLFWwindow *window;
+  VkInstance instance;
 } App;
 
 void initWindow(App *pApp);
-void initVulkan(void);
+void initVulkan(App *pApp);
 void mainLoop(App *pApp);
 void cleanup(App *pApp);
+
+void createInstance(App *pApp);
 
 int main(void) {
   App app = {0};
 
   initWindow(&app);
-  initVulkan();
+  initVulkan(&app);
   mainLoop(&app);
   cleanup(&app);
 
@@ -38,7 +49,9 @@ void initWindow(App *pApp) {
   pApp->window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
 }
 
-void initVulkan(void) {}
+void initVulkan(App *pApp) {
+  createInstance(pApp);
+}
 
 void mainLoop(App *pApp) {
   while (!glfwWindowShouldClose(pApp->window)) {
@@ -47,7 +60,39 @@ void mainLoop(App *pApp) {
 }
 
 void cleanup(App *pApp) {
+  vkDestroyInstance(pApp->instance, NULL);
+
   glfwDestroyWindow(pApp->window);
 
   glfwTerminate();
+}
+
+void createInstance(App *pApp) {
+  VkApplicationInfo appInfo = {
+    .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    .pApplicationName = WIN_TITLE,
+    .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+    .pEngineName = "No Engine",
+    .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+    .apiVersion = VK_API_VERSION_1_0,
+    .pNext = NULL
+  };
+
+  u32 glfwExtensionCount = 0;
+  const char** glfwExtensions;
+
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  VkInstanceCreateInfo createInfo = {
+    .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    .pApplicationInfo = &appInfo,
+    .enabledExtensionCount = glfwExtensionCount,
+    .ppEnabledExtensionNames = glfwExtensions
+  };
+  createInfo.enabledLayerCount = 0;
+
+  if (vkCreateInstance(&createInfo, NULL, &pApp->instance) != VK_SUCCESS) {
+    printf("Failed to create Vulkan Instance\n");
+    exit(1);
+  }
 }
